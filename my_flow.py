9513635bb284f0deb6f29c823918304399c9e4e9
Luke Salomone <ldsalomone@gmail.com>
2024-05-03 21:16:22 -0500

    Create my_flow.py

---
+++ my_flow.py
@@ -0,0 +1,13 @@
+import httpx
+from prefect import flow
+
+
+@flow(log_prints=True)
+def get_repo_info(repo_name: str = "PrefectHQ/prefect"):
+    url = f"https://api.github.com/repos/{repo_name}"
+    response = httpx.get(url)
+    response.raise_for_status()
+    repo = response.json()
+    print(f"{repo_name} repository statistics 🤓:")
+    print(f"Stars: {repo['stargazers_count']}")
+    print(f"Forks: {repo['forks_count']}")
